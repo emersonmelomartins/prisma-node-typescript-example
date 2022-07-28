1443fcc7c2f009aeb99edffe1824e028c945437b
Emerson Melo Martins <emerson25xd@gmail.com>
2022-07-28 00:22:45 -0300

    findOne example

--- server.ts
+++ server.ts
@@ -1,17 +1,39 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const result = await prisma.courses.create({
+async function create() {
+  const course = await prisma.courses.create({
     data: {
       duration: 200,
-      name: "Curso de NodeJS",
+      name: `Curso de Node ${new Date().toLocaleTimeString()}`,
       description: "Curso completo de NodeJS",
     },
   });
 
-  console.log(result);
+  await prisma.courses.create({
+    data: {
+      duration: 123,
+      name: `Curso de Algo ${new Date().toLocaleTimeString()}`,
+      description: "Curso completo de Algo",
+    },
+  });
+
+  console.log(course)
+}
+
+async function findOne() {
+  // SELECT  * FROM COURSES LIMIT 1
+  const course = await prisma.courses.findFirst({
+    take: -1, // retorna o inverso do "findFirst"
+  });
+
+  console.log(course);
+}
+
+async function main() {
+  create();
+  findOne();
 }
 
 main();
